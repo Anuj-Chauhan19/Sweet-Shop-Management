9ea08e528f60665e0818b1d9a61d21634245caaa
Anuj-CHauhan19 <anujdc2007@gmail.com>
2025-07-16 15:40:57 +0530

    feat: add validation to Sweet constructor

--- src/models/Sweet.js
+++ src/models/Sweet.js
@@ -1,10 +1,30 @@
+const { InvalidDataException } = require("../../src/exceptions/CustomExceptions");
+
 class Sweet {
   constructor(id, name, category, price, quantity) {
+    this._validateInput(id, name, category, price, quantity);
     this.id = id;
     this.name = name;
     this.category = category;
     this.price = price;
     this.quantity = quantity;
+  }
+  _validateInput(id, name, category, price, quantity) {
+    if (!id) {
+      throw new InvalidDataException("ID is required");
+    }
+    if (!name || name.trim() === "") {
+      throw new InvalidDataException("Name is required");
+    }
+    if (!category || category.trim() === "") {
+      throw new InvalidDataException("Category is required");
+    }
+    if (price < 0) {
+      throw new InvalidDataException("Price must be positive");
+    }
+    if (quantity < 0) {
+      throw new InvalidDataException("Quantity must be non-negative");
+    }
   }
 }
 
